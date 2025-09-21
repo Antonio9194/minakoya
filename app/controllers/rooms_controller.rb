57a7dab5435b05826878df925cb81b3c4fd8f0eb
Bruno Vetorazo <vetorazo@gmail.com>
2025-09-21 22:25:54 +0900

    Added all data to the view, but just raw, no styling

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,5 +3,16 @@ class RoomsController < ApplicationController
   end
 
   def show
+    set_room
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :description, :price_per_night, :size, :beds, :capacity, :amenities)
+  end
+
+  def set_room
+    @room = Room.find(params[:id])
   end
 end
