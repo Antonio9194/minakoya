1b955c32d16af4394d668ca97b92a501a6f288a3
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-09-22 19:47:28 +0800

    done most of the visual

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,32 @@
 class RoomsController < ApplicationController
-   skip_before_action :authenticate_user!, only: [:index, :show]
-
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
+    @rooms = Room.all
+
+    # Keyword search
+    if params[:query].present?
+      @rooms = @rooms.where("name ILIKE ?", "%#{params[:query]}%")
+    end
+
+    # Guest count 
+    if params[:guests].present?
+      @rooms = @rooms.where("capacity >= ?", params[:guests].to_i)
+    end
+
+    # Availability
+    if params[:check_in].present? && params[:check_out].present?
+      check_in = Date.parse(params[:check_in])
+      check_out = Date.parse(params[:check_out])
+
+      # Find rooms that have overlapping bookings
+      booked_room_ids = Booking.where("start_date < ? AND end_date > ?", check_out, check_in).pluck(:room_id)
+
+      # Exclude those rooms
+      @rooms = @rooms.where.not(id: booked_room_ids)
+    end
   end
 
   def show
+    @room = Room.find(params[:id])
   end
 end
