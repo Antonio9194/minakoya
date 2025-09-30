a5135cc68abbb4bcb2c78466fdb130c1ba706d5b
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-09-30 12:29:33 +0900

    fixed logic fo submitting search data and moved calendar to a partial

--- app/javascript/controllers/booking_form_controller.js
+++ app/javascript/controllers/booking_form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="booking-form"
 export default class extends Controller {
-  static targets = ["calendar", "checkInValue", "checkOutValue"]
+  static targets = ["calendar", "checkInValue", "checkOutValue", "checkInInput", "checkOutInput"]
 
   open(event) {
     this.currentInput = event.currentTarget.dataset.type
@@ -14,8 +14,10 @@ export default class extends Controller {
 
     if (this.currentInput === "check_in") {
       this.checkInValueTarget.textContent = date
+      this.checkInInputTarget.value = date
     } else if (this.currentInput === "check_out") {
       this.checkOutValueTarget.textContent = date
+      this.checkOutInputTarget.value = date
     }
 
     this.calendarTarget.classList.add("d-none") 
