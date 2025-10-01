93996a0cb70da941b8273ceeb7586fef544aa9cb
Bruno Vetorazo <vetorazo@gmail.com>
2025-10-01 22:06:35 +0900

    Added validation for payment status, monetize and added methods to calculate price

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,11 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true
   # TO-DO: Add validation for payment later (if needed)
 
+  # for money-rails to know that total_price_cents is a money field
+  monetize :total_price_cents, with_currency: :jpy
+
+  validates :payment_status, inclusion: { in: %w[pending paid failed refunded] }
+
   scope :upcoming, -> { where("start_date >= ?", Date.today) }
 
   def start_date_cannot_be_in_the_past
@@ -22,6 +27,21 @@ class Booking < ApplicationRecord
   def plan_date_cannot_be_reverse
     if start_date.present? && end_date.present? && end_date < start_date
       errors.add(:end_date, "can't be before starting date")
+    end
+  end
+
+  def number_of_nights
+    return 0 if start_date.nil? || end_date.nil?
+
+    (end_date - start_date).to_i
+  end
+
+  private
+
+  def calculate_total_price
+    if room && start_date && end_date
+      nights = number_of_nights
+      self.total_price_cents = room.price_per_night * nights
     end
   end
 end
