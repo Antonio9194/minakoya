09bf0757bb3014519e710877c198355d60688af0
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-10-07 17:56:40 +0900

    added simple form and started basic style

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -2,6 +2,7 @@ class ContactMessagesController < ApplicationController
    skip_before_action :authenticate_user!, only: [:new, :create]
 
   def new
+    @contact_message = ContactMessage.new
   end
 
   def create
