c37221f849c80fb9882a184792d56c4d0ff0d3fa
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-10-08 09:58:33 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_07_124525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "amenities", force: :cascade do |t|
-    t.string "name"
-    t.string "icon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
@@ -43,15 +36,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_07_124525) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_contact_messages_on_user_id"
-  end
-
-  create_table "room_amenities", force: :cascade do |t|
-    t.bigint "room_id", null: false
-    t.bigint "amenity_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["amenity_id"], name: "index_room_amenities_on_amenity_id"
-    t.index ["room_id"], name: "index_room_amenities_on_room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -89,6 +73,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_07_124525) do
   add_foreign_key "bookings", "rooms"
   add_foreign_key "bookings", "users"
   add_foreign_key "contact_messages", "users"
-  add_foreign_key "room_amenities", "amenities"
-  add_foreign_key "room_amenities", "rooms"
 end
